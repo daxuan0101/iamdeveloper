65c52bae2eff555c77abc1c5903061c801e1e476
daxuan0101 <daxuan0101@naver.com>
2020-11-18 18:22:36 +0900

    hello with tensorflow

--- TEST/hello.py
+++ TEST/hello.py
@@ -1,1 +1,5 @@
-print('hello')
+import tensorflow as tf
+tf.config.list_physical_devices('GPU')
+with tf.compat.v1.Session() as sess:
+    hello = tf.constant('helloWorld')
+    print(sess.run(hello))
